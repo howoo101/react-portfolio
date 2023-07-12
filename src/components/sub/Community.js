87df2248a7aba2c67876af95819a827433c19a54
homean <hohohosky101@gmail.com>
2023-07-12 16:33:02 +0900

    [feature] community  post create,read기능

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,7 +1,86 @@
 import Layout from '../common/Layout';
+import { useEffect, useRef, useState } from 'react';
 
 function Community() {
-	return <Layout name={'Community'}></Layout>;
+	const nextId = useRef(1);
+	const title = useRef(null);
+	const content = useRef(null);
+
+	const [posts, setPosts] = useState([]);
+	const [inputs, setInputs] = useState({
+		title: '',
+		content: '',
+	});
+
+	const onReset = () => {
+		title.current.value = '';
+		content.current.value = '';
+
+		setInputs({ title: '', content: '' });
+
+		title.current.focus();
+	};
+
+	const onChange = (e) => {
+		const { name, value } = e.target;
+		setInputs({ ...inputs, [name]: value });
+	};
+
+	const createPost = () => {
+		if (!title.current.value.trim() || !content.current.value.trim()) {
+			return alert('제목과 본문을 모두 입력하세요.');
+		}
+		const newPost = {
+			id: nextId.current,
+			title: inputs.title,
+			content: inputs.content,
+		};
+
+		setPosts([{ ...newPost }, ...posts]);
+		nextId.current += 1;
+
+		onReset();
+	};
+	useEffect(() => {}, [posts, inputs]);
+
+	return (
+		<Layout name={'Community'}>
+			<div className='inputBox'>
+				<input type='text' placeholder='제목을 입력하세요.' name='title' onChange={onChange} ref={title} />
+				<br />
+				<textarea
+					cols='30'
+					rows='3'
+					placeholder='본문을 입력하세요.'
+					name='content'
+					onChange={onChange}
+					ref={content}
+				></textarea>
+				<br />
+
+				<nav className='btnSet'>
+					<button onClick={onReset}>리셋</button>
+					<button onClick={createPost}>작성</button>
+				</nav>
+			</div>
+			<div className='showBox'>
+				{posts?.map((post, idx) => {
+					return (
+						<article key={idx}>
+							<div className='txt'>
+								<h2>{post.title}</h2>
+								<p>{post.content}</p>
+							</div>
+							<nav className='btnSet'>
+								<button>EDIT</button>
+								<button>DELETE</button>
+							</nav>
+						</article>
+					);
+				})}
+			</div>
+		</Layout>
+	);
 }
 
 export default Community;
