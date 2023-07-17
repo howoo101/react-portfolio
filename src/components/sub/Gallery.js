6ea03f90e986317f4e619515d959162e1270ee47
homean <hohohosky101@gmail.com>
2023-07-17 16:32:56 +0900

    flickr api - 검색, 나의갤러리,기본갤러리

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -8,54 +8,127 @@ function Gallery() {
 	const [items, setItems] = useState([]);
 	const userId = config.flickrUserId;
 
+	const searchInput = useRef(null);
+	const btnSet = useRef(null);
+	const enableEvent = useRef(true);
 	const frame = useRef(null);
-	const counter = useRef(0);
 
-	const getFlickr = useCallback(async (opt) => {
-		const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
-		const key = config.flickrApiKey;
-		const method_interest = 'flickr.interestingness.getList';
-		const method_user = 'flickr.people.getPhotos';
-		const method_search = 'flickr.photos.search';
-		const num = 40;
-		let url = '';
+	const getFlickr = useCallback(
+		async (opt) => {
+			const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
+			const key = config.flickrApiKey;
+			const method_interest = 'flickr.interestingness.getList';
+			const method_user = 'flickr.people.getPhotos';
+			const method_search = 'flickr.photos.search';
+			const num = 40;
+			let url = '';
 
-		if (opt.type === 'interest') url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
-		if (opt.type === 'search')
-			url = `${baseURL}&api_key=${key}&method=${method_search}&per_page=${num}&tags=${opt.tags}`;
-		if (opt.type === 'user')
-			url = `${baseURL}&api_key=${key}&method=${method_user}&per_page=${num}&user_id=${opt.user}`;
+			if (opt.type === 'interest') url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
+			if (opt.type === 'search')
+				url = `${baseURL}&api_key=${key}&method=${method_search}&per_page=${num}&tags=${opt.tags}`;
+			if (opt.type === 'user')
+				url = `${baseURL}&api_key=${key}&method=${method_user}&per_page=${num}&user_id=${opt.user}`;
 
-		const result = await axios.get(url);
+			const result = await axios.get(url);
+			const data = result.data.photos.photo;
+			if (data.length === 0) {
+				setLoader(false);
 
-		setItems(result.data.photos.photo);
-		//외부데이터가 State에 담기고 DOM이 생성되는 순간
-		//모든 img요소를 찾아서 반복처리
-		const imgs = frame.current.querySelectorAll('img');
+				frame.current.classList.add('on');
+				const btnMine = btnSet.current.children;
+				btnMine[1].classList.add('on');
+				getFlickr({ type: 'user', user: userId });
+				enableEvent.current = true;
 
-		counter.current = 0;
-		console.log(imgs);
-		imgs.forEach((img) => {
-			img.onload = () => {
-				++counter.current;
+				return alert('이미지 결과값이 없습니다.');
+			}
 
-				if (counter.current === imgs.length) {
-					setLoader(false);
-					frame.current.classList.add('on');
-				}
-			};
-		});
-	}, []);
+			setItems(data);
+			//외부데이터가 State에 담기고 DOM이 생성되는 순간
+			//모든 img요소를 찾아서 반복처리
+			const imgs = frame.current.querySelectorAll('img');
+
+			let counter = 0;
+
+			imgs.forEach((img) => {
+				img.onload = () => {
+					++counter;
+
+					if (counter === imgs.length) {
+						setLoader(false);
+						frame.current.classList.add('on');
+						enableEvent.current = true;
+					}
+				};
+			});
+		},
+		[userId]
+	);
+
+	const showSearch = (e) => {
+		const tag = searchInput.current.value.trim();
+		if (tag === '') return alert('검색어를 입력하세요.');
+		if (!enableEvent.current) return;
+
+		resetGallery(e);
+		getFlickr({ type: 'search', tags: tag });
+		searchInput.current.value = '';
+	};
+
+	const showInterest = (e) => {
+		//재이벤트, 모션중 재이벤트 방지
+		if (!enableEvent.current) return;
+		if (e.target.classList.contains('on')) return;
+		//기존 갤러리 초기화 함수 호출
+		resetGallery(e);
+		//새로운 데이터로 갤러리 생성 함수 호출
+		getFlickr({ type: 'interest' });
+	};
+
+	const showMine = (e) => {
+		//재이벤트, 모션중 재이벤트 방지
+		if (!enableEvent.current) return;
+		if (e.target.classList.contains('on')) return;
+		//기존 갤러리 초기화 함수 호출
+		resetGallery(e);
+		//새로운 데이터로 갤러리 생성 함수 호출
+		getFlickr({ type: 'user', user: userId });
+	};
+
+	//기존 갤러리 초기화 함수
+	const resetGallery = (e) => {
+		const btns = btnSet.current.querySelectorAll('button');
+		btns.forEach((el) => el.classList.remove('on'));
+		e.target.classList.add('on');
+		enableEvent.current = false;
+		setLoader(true);
+		frame.current.classList.remove('on');
+	};
 
 	useEffect(() => {
-		getFlickr({ type: 'interest' });
-	}, [getFlickr]);
+		getFlickr({ type: 'user', user: userId });
+	}, [getFlickr, userId]);
 
 	return (
 		<Layout name={'Gallery'}>
+			<div className='btnSet' ref={btnSet}>
+				<button onClick={showInterest}>Interest Gallery</button>
+				<button className='on' onClick={showMine}>
+					My Gallery
+				</button>
+			</div>
+
 			<div className='searchBox'>
-				<input type='text' id='search' placeholder='검색어 입력' />
-				<button className='searchBtn'>Search</button>
+				<input
+					type='text'
+					id='search'
+					placeholder='검색어 입력'
+					ref={searchInput}
+					onKeyPress={(e) => e.key === 'Enter' && showSearch(e)}
+				/>
+				<button className='searchBtn' onClick={showSearch}>
+					Seach
+				</button>
 			</div>
 			<div className='frame' ref={frame}>
 				<Masonry elementType={'div'} options={{ transitionDuration: '0.5s' }}>
