98e81dc320291707e62098ad67ed170972da1033
ho <hohohosky101@gmail.com>
2023-07-26 18:12:57 +0900

    메인페이지 - 헤더 스크롤시 헤더 배경 추가

--- src/components/common/Main.js
+++ src/components/common/Main.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import Header from './Header';
 import Visual from '../main/Visual';
 import Choice from '../main/Choice';
 import Diary from '../main/Diary';
 import Accessory from '../main/Accessory';
+import Scrolls from './Scrolls';
 
 function Main({ menu }) {
+	const mainRef = useRef(null);
+
+	const activeHeader = useCallback(() => {
+		const header = mainRef.current.querySelector('#header');
+
+		const scroll = window.scrollY;
+
+		header.classList.remove('on');
+		header.classList.remove('off');
+
+		scroll > 0 ? header.classList.add('on') : header.classList.add('off');
+	}, []);
+
+	useEffect(() => {
+		window.addEventListener('scroll', activeHeader);
+	});
 	return (
-		<main>
+		<main ref={mainRef}>
 			<Header type={'main'} menu={menu} />
 			<Visual />
 			<Choice />
